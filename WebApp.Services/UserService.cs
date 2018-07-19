0ca16272b267f4dcb8c8ce67df94ca7d65d93703
谢宁波 <xienb@live.com>
2018-07-19 11:56:42 +0800

    初始

---
+++ WebApp.Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.Common;
+using Dapper;
+using System.Collections.Generic;
+
+namespace WebApp.Services
+{
+    public class UserService
+    {
+        DbConnection ServerDbConnection = new MySqlConnection("server=192.168.1.79;user id=root; password=123456; database=testdb; pooling=false;SslMode=none");
+
+        public int AddUser(string name, string city)
+        {
+            string sql = "insert into student(name,city,cdt) values(@name,@city,@cdt)";
+            return ServerDbConnection.Execute(sql, new { name = name, city = city, cdt = DateTime.Now });
+        }
+
+        public IList<dynamic> GetUserList()
+        {
+            return ServerDbConnection.Query("select * from student").AsList();
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult AddStudent(string name, string city)
+        {
+            UserService service = new UserService();
+            int ret =  service.AddUser(name, city);
+            return Json(ret);
+        }
+
+        [HttpGet]
+        public JsonResult GetStudent()
+        {
+            UserService service = new UserService();
+            return Json(service.GetUserList());
+        }
+    }
+}
